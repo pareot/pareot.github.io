af440ba30a5ba378611cdb099418fc736c326c3d
pareot <55153435+pareot@users.noreply.github.com>
2019-10-30 15:50:24 -0700

    a bunch of thing

--- art75project3/sketch.js
+++ art75project3/sketch.js
@@ -3,14 +3,9 @@ function setup() {
 }
 
 function draw(){
-  background(220);
-  ellipse(120, 130, 360, 360);
-}
+  background("blue"");
 
-function setup() {
-  createCanvas(800, 800);
-function draw() {
-  background(650)
+  ellipse(120, 130, 360, 360);
+
   arc(90, 60, 90, 90, PI+HALF_PI+QUARTER_PI, QUARTER_PI);
 }
-	
