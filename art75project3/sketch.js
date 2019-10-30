8058ea25c72a6baf4f86bd3ece802b2a15e1e568
pareot <55153435+pareot@users.noreply.github.com>
2019-10-30 15:36:08 -0700

    changed folder location

---
+++ art75project3/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw(){
+  background(220);
+  ellipse(120, 130, 360, 360);
+}
+
+function setup() {
+  createCanvas(800, 800);
+function draw() {
+  background(650)
+  arc(90, 60, 90, 90, PI+HALF_PI+QUARTER_PI, QUARTER_PI);
+}
+	
