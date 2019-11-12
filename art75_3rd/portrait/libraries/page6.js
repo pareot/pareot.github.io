56c6533065f191f9d0873b04cf6a3c8dbdbea81e
pareot <55153435+pareot@users.noreply.github.com>
2019-11-12 10:36:57 -0800

    update page6.js

--- art75_3rd/portrait/libraries/page6.js
+++ art75_3rd/portrait/libraries/page6.js
@@ -1,16 +1,4 @@
 saveCount = 0;
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(0);
-}
-
-function draw() {
-  fill(200, 200, 200, 20);
-  noStroke();
-  ellipse(mouseX, mouseY, 30, 30);
-}
-
 function keyPressed() {
   save("screenshot" + saveCount + ".png");
   saveCount++;
