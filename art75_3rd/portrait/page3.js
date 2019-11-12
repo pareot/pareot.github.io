8e187e3173db56e1c3c58065c277439e435bfef9
pareot <55153435+pareot@users.noreply.github.com>
2019-11-12 10:03:49 -0800

    alex

--- art75_3rd/portrait/page3.js
+++ art75_3rd/portrait/page3.js
@@ -1,14 +1,19 @@
-function preload() {
-  img = loadImage();
-}
-
 saveCount = 0;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(img);
+  background(0);
+}
+
+function draw() {
+  fill(200, 200, 200, 20);
+  noStroke();
+  ellipse(mouseX, mouseY, 30, 30);
 }
 
 function keyPressed() {
-  save("screenshot" + saveCount + ".jpg");
-  saveCount++;
+  if (key == 'x') {
+    save("screenshot" + saveCount + ".png");
+    saveCount++;
+  }
 }
