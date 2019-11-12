82d4843d30096058a6bf9bb5a409c0c4c39a51c6
pareot <55153435+pareot@users.noreply.github.com>
2019-11-12 10:11:26 -0800

    alex

--- art75_3rd/portrait/page1.js
+++ art75_3rd/portrait/page1.js
@@ -1,18 +1,16 @@
-saveCount = 0;
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- PLEASE NO CHANGES BELOW THIS LINE (UNTIL I SAY SO) -->
+  <script language="javascript" type="text/javascript" src="page6.js"></script>
+  <!-- OK, YOU CAN MAKE CHANGES BELOW THIS LINE AGAIN -->
+  <!-- This line removes any default padding and style.
+       You might only need one of these values set. -->
+  <style> body { padding: 0; margin: 0; } </style>
+</head>
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(0);
-}
+<body>
+</body>
 
-function draw() {
-  fill(200, 200, 200, 20);
-  noStroke();
-  ellipse(mouseX, mouseY, 30, 30);
-}
-
-function keyPressed() {
-  save("screenshot" + saveCount + ".png");
-  saveCount++;
-  }
-}
+</html>
