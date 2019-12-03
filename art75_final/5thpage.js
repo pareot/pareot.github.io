96f052b264c6728b5d980b84a56d1e879a9c3142
pareot <55153435+pareot@users.noreply.github.com>
2019-12-03 09:15:48 -0800

    Create 5thpage.js

---
+++ art75_final/5thpage.js
@@ -0,0 +1,12 @@
+function setup () {
+  createCanvas(windowWidth,windowHeight);
+}
+
+saveCount = 10;
+
+function keyPressed() {
+  if(keyIsPressed) {
+    save("gotyourpicture" + saveCount + ".png");
+    saveCount++;
+  }
+}
