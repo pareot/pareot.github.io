c8c6885210984fafd612d984c6684b15240e87c2
pareot <55153435+pareot@users.noreply.github.com>
2019-12-12 12:32:48 -0800

    Update example.js

--- art75project3/example.js
+++ art75project3/example.js
@@ -6,24 +6,15 @@ function setup() {
 
 function draw() {
    background("gray");
-
-   noStroke();
-   fill("black");
-   ellipse(264,377,33,33);
-   fill("gray");
-   rect(219,257,90,120);
-   fill("blue");
-   rect(219,274,90,6);
-
-
+   
    fill("black");
    ellipse(276,155,45,45);
    fill("red");
    ellipse(288,150,14,14);
    fill("white");
    ellipse(288,150,3,3);
-   fill("orange");
-   ellipse(263,148,5,5);
-   ellipse(296,130,4,4);
-   ellipse(305,162,3,3);
+   fill("yellow");
+   rect(263,148,5,5);
+   rect(296,130,4,4);
+   rect(305,162,3,3);
    }
