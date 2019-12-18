0e2d781203634e14ed7d9288ac38f19ba2bc06ae
pareot <55153435+pareot@users.noreply.github.com>
2019-12-18 10:37:26 -0800

    Update phlepts.js

--- art74_phle_photoshop/phlepts.js
+++ art74_phle_photoshop/phlepts.js
@@ -73,7 +73,7 @@ function newkeyChoice(toolChoice) { //toolchoice is the key that was pressed
   } else if (toolChoice == '9') { // ninth tool
 
     noStroke();
-    fill(100, 100, 0, 80);
+    fill(0, 100, 100, 80);
     rect(mouseX, mouseY, 140, 140);
   } else if (toolChoice == '0') { // tenth tool - random
 
