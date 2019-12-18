05a4c2e0a51bf2f3edfeafead7598e7707c69f3e
pareot <55153435+pareot@users.noreply.github.com>
2019-12-18 11:11:47 -0800

    Create face_sketch.js

---
+++ art74_final/face_sketch.js
@@ -0,0 +1,7 @@
+var Xpos; //Face horizontal position
+var Ypos; //Face verticel position
+var Scale; //Face scale
+var LeftEBH; //Left Eyebrow Height
+var RightEBH; //Right Eyebrow Height
+var LeftEyeOpen; //Openness of left-eye
+var RightEyeOpen; //Opennes of right-Eyebrow
