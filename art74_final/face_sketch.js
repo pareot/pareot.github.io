0f12f59f035b4027335c77135ddf5c116e7d8e19
pareot <55153435+pareot@users.noreply.github.com>
2019-12-18 20:15:23 -0800

    Update face_sketch.js

--- art74_final/face_sketch.js
+++ art74_final/face_sketch.js
@@ -19,20 +19,20 @@ function draw () {
   stroke(10);
   ellipse(posx, posy, random(-240, 240), random (-180, 180));
 
-  rectMode(RADIUS); //drawing left eyebrow
-  noFill();
-  stroke(5);
-  rect(leftebh, leftebh, 50, 50);
+//  rectMode(RADIUS); //drawing left eyebrow
+//  noFill();
+//  stroke(5);
+//  rect(leftebh, leftebh, 50, 50);
 
-  rectMode(RADIUS); //drawing right eyebrow
-  noFill();
-  stroke(5);
-  rect(rightebh, rightebh, 50, 50);
+//  rectMode(RADIUS); //drawing right eyebrow
+//  noFill();
+//  stroke(5);
+//  rect(rightebh, rightebh, 50, 50);
 
-  ellipseMode(RADIUS); //drawing left eye
-  noFill();
-  stroke(3);
-  ellipse(posx);
+//  ellipseMode(RADIUS); //drawing left eye
+//  noFill();
+//  stroke(3);
+//  ellipse(posx);
   }
 
 }
